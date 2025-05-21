3b128f13d232c0a9d7bd62094329358a6b853b43
MarkBits1301 <23ecuos119@ddu.ac.in>
2025-05-21 12:59:26 +0530

    fffffff

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://moatkvzafvqklvaruclx.supabase.co'
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vYXRrdnphZnZxa2x2YXJ1Y2x4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY2MjYxNjYsImV4cCI6MjA2MjIwMjE2Nn0.gWBVhjIQhWN3HgmfwZfxDcLqEp0ZF9lHPPhVDcEccxU'
+const SUPABASE_URL = 'https://detpjnajemrsvntqccnh.supabase.co'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRldHBqbmFqZW1yc3ZudHFjY25oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUxMzYwNzAsImV4cCI6MjA2MDcxMjA3MH0.akVvkUxTg64LTT0c6uVH1CiWk-AUVLYlwdZiyjnkuKg'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
